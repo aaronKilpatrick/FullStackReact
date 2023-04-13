04d36e365c4daa2170632ec6607d0fc4664a171a
Aaron Kilpatrick <kilpatrick.aaron92@gmail.com>
2023-04-13 16:55:09 +1000

    Complete part01.b

---
+++ part01/a/part1/src/App.js
@@ -0,0 +1,34 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      Greeting app created by 
+      <a href='https://github.com/aaronkilpatrick'>
+        Aaron Kilpatrick
+      </a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age  = 10
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26+10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
